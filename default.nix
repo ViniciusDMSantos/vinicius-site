28b483427321dbee274e89ed85eed59795c618e1
Vinicius Santos <123326846+ViniciusDMSantos@users.noreply.github.com>
2024-12-16 19:21:10 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,18 @@
 { stdenv, lib, fetchFromGitHub, hugo, go, git }:
 
+let
+  theme = stdenv.mkDerivation rec {
+    name = "theme";
+    src = fetchFromGitHub {       
+      owner = "adityatelange";
+      repo = "hugo-PaperMod";
+      rev = "3e53621";
+      hash = lib.fakeSha256;
+     installPhase = ''
+     cp -r ./* $out/*
+    '';
+  };
+in
 stdenv.mkDerivation rec {
   name = "vinicius-site";
   src = ./.;
